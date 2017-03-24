ae4f7a88356c55b13130d4036a81ee68c5fcc7b8
salwa <salwaoudad@gmail.com>
2017-03-24 16:05:31 +0100

    slider slider full screen

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.slider').slider();
+
+    // Pause slider
+    $('.slider').slider('pause');
+    // Start slider
+    $('.slider').slider('start');
+    // Next slide
+    $('.slider').slider('next');
+    // Previous slide
+    $('.slider').slider('prev');
+
+    $('.carousel').carousel();
+
+    $('.carousel').carousel('next');
+    $('.carousel').carousel('next', 3); // Move next n times.
+    // Previous slide
+    $('.carousel').carousel('prev');
+    $('.carousel').carousel('prev', 4); // Move prev n times.
+    // Set to nth slide
+    $('.carousel').carousel('set', 4);
+});
